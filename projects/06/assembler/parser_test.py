39dd684f4baf024960e5ae5964b50479f023cab4
Khayrat Glende <khayrat.glende@neofonie.de>
2020-04-18 20:35:40 +0200

    assembler w/o symbols.

--- projects/06/assembler/parser_test.py
+++ projects/06/assembler/parser_test.py
@@ -1,6 +1,7 @@
 from parser import Parser
+from code import Code
 
-def test(asm_file):
+def test_parser(asm_file):
     p=Parser(asm_file)
 
     print("run parser.....")
@@ -17,3 +18,23 @@ def test(asm_file):
 
         p.advance()
     print("... finished run parser")
+
+def test_parser_with_code(asm_file):
+    p=Parser(asm_file)
+    c=Code()
+
+    print("run parser with code.....")
+    while p.hasMoreCommands():
+        if p.commandType() in ['A_COMMAND', 'L_COMMAND']:
+            print("symbol: '%s'" % p.symbol())
+
+        if p.commandType() in ['C_COMMAND']:
+            print("dest: '%s'" % c.dest(p.dest()))
+            print("comp: '%s'" % c.comp(p.comp()))
+            print("jump: '%s'" % c.jump(p.jump()))
+
+        p.advance()
+
+def test_assembler_wo_symbols(asm_file):
+    a=Assembler(asm_file)
+    a.assemble()
