9c0cc2252de30aeb8b56f0a7d2f05791a34332bd
Khayrat Glende <khayrat.glende@neofonie.de>
2023-05-28 08:33:09 +0200

    removed old.

--- projects/06/assembler/test_assembler.py
+++
@@ -1,9 +0,0 @@
-from assembler import Assembler
-
-def test_firstPass():
-    a=Assembler('firstPass.asm')
-    a._firstPass()
-    symbols = ['i', 'loop', 'END', '4711', 'sum'] 
-    for s in symbols:
-        if a.st.contains(s):
-            print("'%s': '%s'" % (s, a.st.getAddress(s)))
